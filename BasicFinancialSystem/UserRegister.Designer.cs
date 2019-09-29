f8ea590b58f2e230bb0a7aaca51a2f6299a6e458
daviskooo <daviferreiradelima.contato@gmail.com>
2019-09-29 20:18:26 -0300

    Adding inputs, labels and buttons to User Register Form. DB created and updated with 2 tables (User and Customer).

--- BasicFinancialSystem/UserRegister.Designer.cs
+++ BasicFinancialSystem/UserRegister.Designer.cs
@@ -28,19 +28,251 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.userRegisterCodeLabel = new System.Windows.Forms.Label();
+            this.userRegisterCodeText = new System.Windows.Forms.TextBox();
+            this.userRegisterNameLabel = new System.Windows.Forms.Label();
+            this.userRegisterNameText = new System.Windows.Forms.TextBox();
+            this.userRegisterLevelLabel = new System.Windows.Forms.Label();
+            this.userRegisterLevelText = new System.Windows.Forms.TextBox();
+            this.userRegistrationLoginLabel = new System.Windows.Forms.Label();
+            this.userRegisterLoginText = new System.Windows.Forms.TextBox();
+            this.userRegisterPasswordLabel = new System.Windows.Forms.Label();
+            this.userRegisterPasswordText = new System.Windows.Forms.TextBox();
+            this.userRegisterPreviousButton = new System.Windows.Forms.Button();
+            this.userRegisterNextButton = new System.Windows.Forms.Button();
+            this.userRegisterNewButton = new System.Windows.Forms.Button();
+            this.userRegisterEditButton = new System.Windows.Forms.Button();
+            this.userRegisterDeleteButton = new System.Windows.Forms.Button();
+            this.userRegisterSaveButton = new System.Windows.Forms.Button();
+            this.userRegisterCancelButton = new System.Windows.Forms.Button();
+            this.userRegisterSearchButton = new System.Windows.Forms.Button();
+            this.userRegisterPrintButton = new System.Windows.Forms.Button();
+            this.userRegisterExitButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // userRegisterCodeLabel
+            // 
+            this.userRegisterCodeLabel.AutoSize = true;
+            this.userRegisterCodeLabel.Location = new System.Drawing.Point(60, 65);
+            this.userRegisterCodeLabel.Name = "userRegisterCodeLabel";
+            this.userRegisterCodeLabel.Size = new System.Drawing.Size(35, 13);
+            this.userRegisterCodeLabel.TabIndex = 0;
+            this.userRegisterCodeLabel.Text = "Code:";
+            // 
+            // userRegisterCodeText
+            // 
+            this.userRegisterCodeText.Location = new System.Drawing.Point(101, 62);
+            this.userRegisterCodeText.Name = "userRegisterCodeText";
+            this.userRegisterCodeText.Size = new System.Drawing.Size(100, 20);
+            this.userRegisterCodeText.TabIndex = 1;
+            // 
+            // userRegisterNameLabel
+            // 
+            this.userRegisterNameLabel.AutoSize = true;
+            this.userRegisterNameLabel.Location = new System.Drawing.Point(57, 104);
+            this.userRegisterNameLabel.Name = "userRegisterNameLabel";
+            this.userRegisterNameLabel.Size = new System.Drawing.Size(38, 13);
+            this.userRegisterNameLabel.TabIndex = 2;
+            this.userRegisterNameLabel.Text = "Name:";
+            // 
+            // userRegisterNameText
+            // 
+            this.userRegisterNameText.Location = new System.Drawing.Point(102, 104);
+            this.userRegisterNameText.Name = "userRegisterNameText";
+            this.userRegisterNameText.Size = new System.Drawing.Size(438, 20);
+            this.userRegisterNameText.TabIndex = 3;
+            // 
+            // userRegisterLevelLabel
+            // 
+            this.userRegisterLevelLabel.AutoSize = true;
+            this.userRegisterLevelLabel.Location = new System.Drawing.Point(57, 146);
+            this.userRegisterLevelLabel.Name = "userRegisterLevelLabel";
+            this.userRegisterLevelLabel.Size = new System.Drawing.Size(36, 13);
+            this.userRegisterLevelLabel.TabIndex = 4;
+            this.userRegisterLevelLabel.Text = "Level:";
+            // 
+            // userRegisterLevelText
+            // 
+            this.userRegisterLevelText.Location = new System.Drawing.Point(102, 143);
+            this.userRegisterLevelText.Name = "userRegisterLevelText";
+            this.userRegisterLevelText.Size = new System.Drawing.Size(37, 20);
+            this.userRegisterLevelText.TabIndex = 5;
+            // 
+            // userRegistrationLoginLabel
+            // 
+            this.userRegistrationLoginLabel.AutoSize = true;
+            this.userRegistrationLoginLabel.Location = new System.Drawing.Point(60, 185);
+            this.userRegistrationLoginLabel.Name = "userRegistrationLoginLabel";
+            this.userRegistrationLoginLabel.Size = new System.Drawing.Size(36, 13);
+            this.userRegistrationLoginLabel.TabIndex = 6;
+            this.userRegistrationLoginLabel.Text = "Login:";
+            // 
+            // userRegisterLoginText
+            // 
+            this.userRegisterLoginText.Location = new System.Drawing.Point(103, 185);
+            this.userRegisterLoginText.Name = "userRegisterLoginText";
+            this.userRegisterLoginText.Size = new System.Drawing.Size(100, 20);
+            this.userRegisterLoginText.TabIndex = 7;
+            // 
+            // userRegisterPasswordLabel
+            // 
+            this.userRegisterPasswordLabel.AutoSize = true;
+            this.userRegisterPasswordLabel.Location = new System.Drawing.Point(60, 223);
+            this.userRegisterPasswordLabel.Name = "userRegisterPasswordLabel";
+            this.userRegisterPasswordLabel.Size = new System.Drawing.Size(56, 13);
+            this.userRegisterPasswordLabel.TabIndex = 8;
+            this.userRegisterPasswordLabel.Text = "Password:";
+            // 
+            // userRegisterPasswordText
+            // 
+            this.userRegisterPasswordText.Location = new System.Drawing.Point(120, 220);
+            this.userRegisterPasswordText.Name = "userRegisterPasswordText";
+            this.userRegisterPasswordText.Size = new System.Drawing.Size(236, 20);
+            this.userRegisterPasswordText.TabIndex = 9;
+            // 
+            // userRegisterPreviousButton
+            // 
+            this.userRegisterPreviousButton.Location = new System.Drawing.Point(60, 325);
+            this.userRegisterPreviousButton.Name = "userRegisterPreviousButton";
+            this.userRegisterPreviousButton.Size = new System.Drawing.Size(75, 23);
+            this.userRegisterPreviousButton.TabIndex = 10;
+            this.userRegisterPreviousButton.Text = "Previous";
+            this.userRegisterPreviousButton.UseVisualStyleBackColor = true;
+            // 
+            // userRegisterNextButton
+            // 
+            this.userRegisterNextButton.Location = new System.Drawing.Point(141, 325);
+            this.userRegisterNextButton.Name = "userRegisterNextButton";
+            this.userRegisterNextButton.Size = new System.Drawing.Size(75, 23);
+            this.userRegisterNextButton.TabIndex = 11;
+            this.userRegisterNextButton.Text = "Next";
+            this.userRegisterNextButton.UseVisualStyleBackColor = true;
+            // 
+            // userRegisterNewButton
+            // 
+            this.userRegisterNewButton.Location = new System.Drawing.Point(222, 325);
+            this.userRegisterNewButton.Name = "userRegisterNewButton";
+            this.userRegisterNewButton.Size = new System.Drawing.Size(75, 23);
+            this.userRegisterNewButton.TabIndex = 12;
+            this.userRegisterNewButton.Text = "New";
+            this.userRegisterNewButton.UseVisualStyleBackColor = true;
+            // 
+            // userRegisterEditButton
+            // 
+            this.userRegisterEditButton.Location = new System.Drawing.Point(304, 325);
+            this.userRegisterEditButton.Name = "userRegisterEditButton";
+            this.userRegisterEditButton.Size = new System.Drawing.Size(75, 23);
+            this.userRegisterEditButton.TabIndex = 13;
+            this.userRegisterEditButton.Text = "Edit";
+            this.userRegisterEditButton.UseVisualStyleBackColor = true;
+            // 
+            // userRegisterDeleteButton
+            // 
+            this.userRegisterDeleteButton.Location = new System.Drawing.Point(385, 325);
+            this.userRegisterDeleteButton.Name = "userRegisterDeleteButton";
+            this.userRegisterDeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.userRegisterDeleteButton.TabIndex = 14;
+            this.userRegisterDeleteButton.Text = "Delete";
+            this.userRegisterDeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // userRegisterSaveButton
+            // 
+            this.userRegisterSaveButton.Location = new System.Drawing.Point(60, 355);
+            this.userRegisterSaveButton.Name = "userRegisterSaveButton";
+            this.userRegisterSaveButton.Size = new System.Drawing.Size(75, 23);
+            this.userRegisterSaveButton.TabIndex = 15;
+            this.userRegisterSaveButton.Text = "Save";
+            this.userRegisterSaveButton.UseVisualStyleBackColor = true;
+            // 
+            // userRegisterCancelButton
+            // 
+            this.userRegisterCancelButton.Location = new System.Drawing.Point(142, 355);
+            this.userRegisterCancelButton.Name = "userRegisterCancelButton";
+            this.userRegisterCancelButton.Size = new System.Drawing.Size(75, 23);
+            this.userRegisterCancelButton.TabIndex = 16;
+            this.userRegisterCancelButton.Text = "Cancel";
+            this.userRegisterCancelButton.UseVisualStyleBackColor = true;
+            // 
+            // userRegisterSearchButton
+            // 
+            this.userRegisterSearchButton.Location = new System.Drawing.Point(224, 355);
+            this.userRegisterSearchButton.Name = "userRegisterSearchButton";
+            this.userRegisterSearchButton.Size = new System.Drawing.Size(75, 23);
+            this.userRegisterSearchButton.TabIndex = 17;
+            this.userRegisterSearchButton.Text = "Search";
+            this.userRegisterSearchButton.UseVisualStyleBackColor = true;
+            // 
+            // userRegisterPrintButton
+            // 
+            this.userRegisterPrintButton.Location = new System.Drawing.Point(306, 355);
+            this.userRegisterPrintButton.Name = "userRegisterPrintButton";
+            this.userRegisterPrintButton.Size = new System.Drawing.Size(75, 23);
+            this.userRegisterPrintButton.TabIndex = 18;
+            this.userRegisterPrintButton.Text = "Print";
+            this.userRegisterPrintButton.UseVisualStyleBackColor = true;
+            // 
+            // userRegisterExitButton
+            // 
+            this.userRegisterExitButton.Location = new System.Drawing.Point(388, 355);
+            this.userRegisterExitButton.Name = "userRegisterExitButton";
+            this.userRegisterExitButton.Size = new System.Drawing.Size(75, 23);
+            this.userRegisterExitButton.TabIndex = 19;
+            this.userRegisterExitButton.Text = "Exit";
+            this.userRegisterExitButton.UseVisualStyleBackColor = true;
             // 
             // UserRegisterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userRegisterExitButton);
+            this.Controls.Add(this.userRegisterPrintButton);
+            this.Controls.Add(this.userRegisterSearchButton);
+            this.Controls.Add(this.userRegisterCancelButton);
+            this.Controls.Add(this.userRegisterSaveButton);
+            this.Controls.Add(this.userRegisterDeleteButton);
+            this.Controls.Add(this.userRegisterEditButton);
+            this.Controls.Add(this.userRegisterNewButton);
+            this.Controls.Add(this.userRegisterNextButton);
+            this.Controls.Add(this.userRegisterPreviousButton);
+            this.Controls.Add(this.userRegisterPasswordText);
+            this.Controls.Add(this.userRegisterPasswordLabel);
+            this.Controls.Add(this.userRegisterLoginText);
+            this.Controls.Add(this.userRegistrationLoginLabel);
+            this.Controls.Add(this.userRegisterLevelText);
+            this.Controls.Add(this.userRegisterLevelLabel);
+            this.Controls.Add(this.userRegisterNameText);
+            this.Controls.Add(this.userRegisterNameLabel);
+            this.Controls.Add(this.userRegisterCodeText);
+            this.Controls.Add(this.userRegisterCodeLabel);
             this.Name = "UserRegisterForm";
-            this.Text = "UserRegister";
+            this.Text = "User Registration";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label userRegisterCodeLabel;
+        private System.Windows.Forms.TextBox userRegisterCodeText;
+        private System.Windows.Forms.Label userRegisterNameLabel;
+        private System.Windows.Forms.TextBox userRegisterNameText;
+        private System.Windows.Forms.Label userRegisterLevelLabel;
+        private System.Windows.Forms.TextBox userRegisterLevelText;
+        private System.Windows.Forms.Label userRegistrationLoginLabel;
+        private System.Windows.Forms.TextBox userRegisterLoginText;
+        private System.Windows.Forms.Label userRegisterPasswordLabel;
+        private System.Windows.Forms.TextBox userRegisterPasswordText;
+        private System.Windows.Forms.Button userRegisterPreviousButton;
+        private System.Windows.Forms.Button userRegisterNextButton;
+        private System.Windows.Forms.Button userRegisterNewButton;
+        private System.Windows.Forms.Button userRegisterEditButton;
+        private System.Windows.Forms.Button userRegisterDeleteButton;
+        private System.Windows.Forms.Button userRegisterSaveButton;
+        private System.Windows.Forms.Button userRegisterCancelButton;
+        private System.Windows.Forms.Button userRegisterSearchButton;
+        private System.Windows.Forms.Button userRegisterPrintButton;
+        private System.Windows.Forms.Button userRegisterExitButton;
     }
 }
