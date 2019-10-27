f8f4757f7651c2c7b53c9204678fc9ed80c8704d
daviskooo <daviferreiradelima.contato@gmail.com>
2019-10-27 10:23:01 -0300

    Adding some fancy style to Login Form.

--- Basic Financial System/Basic Financial System/Login.Designer.cs
+++ Basic Financial System/Basic Financial System/Login.Designer.cs
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.title = new System.Windows.Forms.Label();
+            this.subtitle = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.rightPanel = new System.Windows.Forms.Panel();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Karla", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(47, 46);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(90, 34);
+            this.title.TabIndex = 0;
+            this.title.Text = "Login";
+            // 
+            // subtitle
+            // 
+            this.subtitle.AutoSize = true;
+            this.subtitle.Font = new System.Drawing.Font("Karla", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtitle.Location = new System.Drawing.Point(50, 90);
+            this.subtitle.Name = "subtitle";
+            this.subtitle.Size = new System.Drawing.Size(214, 30);
+            this.subtitle.TabIndex = 1;
+            this.subtitle.Text = "Lorem ipsum adnet calabrese frité\r\nLorem ipsum adnet calabrese frité\r\n";
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Karla", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.Location = new System.Drawing.Point(50, 155);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(86, 18);
+            this.usernameLabel.TabIndex = 2;
+            this.usernameLabel.Text = "User name:";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(53, 182);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(323, 23);
+            this.usernameTextBox.TabIndex = 3;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Karla", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(50, 226);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(77, 18);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(53, 255);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(323, 23);
+            this.passwordTextBox.TabIndex = 5;
+            // 
+            // rightPanel
+            // 
+            this.rightPanel.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.rightPanel.Location = new System.Drawing.Point(432, -2);
+            this.rightPanel.Name = "rightPanel";
+            this.rightPanel.Size = new System.Drawing.Size(399, 411);
+            this.rightPanel.TabIndex = 6;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(53, 310);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(323, 23);
+            this.loginButton.TabIndex = 7;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(108, 339);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(216, 23);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(829, 408);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.rightPanel);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.subtitle);
+            this.Controls.Add(this.title);
+            this.Font = new System.Drawing.Font("Karla", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label subtitle;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Panel rightPanel;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button exitButton;
     }
 }
